13d60abc54abac5cc24230d4e56eabc61bd2c4e2
TC Juan <tcjuan@localhost.localdomain>
2014-08-08 08:08:25 +0800

    add store id to activator

--- db/migrate/20140807145135_add_store_id_to_activators.rb
+++ db/migrate/20140807145135_add_store_id_to_activators.rb
@@ -1,4 +1,15 @@
 class AddStoreIdToActivators < ActiveRecord::Migration
-  def change
+  def self.up
+   
+   change_table :spree_activators do |t|
+      t.string :store_id
+    end
+  end
+
+  def self.down
+   change_table :spree_activators do |t|
+      t.remove :store_id
+    end
+    
   end
 end
