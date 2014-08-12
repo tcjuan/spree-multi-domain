6e5e51a83b8c0b25016aac3c4dbf05083fb92f78
TC Juan <tcjuan@localhost.localdomain>
2014-08-12 08:25:45 +0800

    fix for taxonomy selection for product

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,6 +1,9 @@
 Spree::ProductsController.class_eval do
   before_filter :can_show_product, :only => :show
 
+
+
+
   private
   def can_show_product
     @product ||= Spree::Product.find_by_permalink!(params[:id])
