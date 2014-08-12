240feef62ed748da417daa42c623b16e1fdad8cd
TC Juan <tcjuan@localhost.localdomain>
2014-08-12 18:20:22 +0800

    add user_controller_decorator to permit store_id param

--- lib/spree/controller_helpers/strong_parameters_decorator.rb
+++
@@ -1,7 +0,0 @@
-Spree::Core::ControllerHelpers::StrongParameters.module_eval do
-  def permitted_user_attributes
-          permitted_attributes.user_attributes + [
-           :store_id_attributes => permitted_store_id_attributes
-          ]
-  end
-end
