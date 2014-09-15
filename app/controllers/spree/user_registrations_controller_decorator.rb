db8b8e87edfe2939e6ca1e2a2ffe6e55faa9ddd7
TC Juan <tcjuan@localhost.localdomain>
2014-09-16 00:16:50 +0800

    add multi user for mulit domain

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -1,6 +1,26 @@
 Spree::UserRegistrationsController.class_eval do
  
-  
+
+  def new
+    super
+    @user = resource
+  end
+
+  # POST /resource/sign_up
+  def create
+    
+    @user = build_resource(spree_user_params)
+    if @user.save!
+      set_flash_message(:notice, :signed_up)
+      sign_in(:spree_user, @user)
+      session[:spree_user_signup] = true
+      associate_user
+      respond_with resource, location: after_sign_up_path_for(resource)
+    else
+      clean_up_passwords(resource)
+      render :new
+    end
+  end
       
   private
   
@@ -10,7 +30,8 @@ Spree::UserRegistrationsController.class_eval do
       params.require(:spree_user).permit(Spree::PermittedAttributes.user_attributes.push :store_id)
     end
     
-       
+    
+ 
 end
 
 
