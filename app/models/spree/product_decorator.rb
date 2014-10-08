a08cb67baa5815fd8eccf73321814661f3ecb36d
TC Juan <tcjuan@localhost.localdomain>
2014-10-08 22:16:20 +0800

    add search filter for available_off

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -13,6 +13,7 @@ Spree::Product.class_eval do
     {:stores => {:as => :store,  :multivalued => true}} ] 
   has_and_belongs_to_many :stores, :join_table => 'spree_products_stores'
   scope :by_store, lambda {|store| joins(:stores).where("spree_products_stores.store_id = ?", store)}
+  scope :by_available , where( ' ? < available_off' ,Time.now)
   
   
   def _available_on
