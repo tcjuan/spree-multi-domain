651dd115a5bb5771464e3891840d83a8ac6e1d94
TC Juan <tcjuan@localhost.localdomain>
2014-10-11 21:38:50 +0800

    update store type tp mulit value

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,14 +3,14 @@ Spree::Product.class_eval do
   PRODUCT_SOLR_FIELDS << :permalink
   PRODUCT_SOLR_FIELDS << {:_available_on => {:as=> :available_on }}
   PRODUCT_SOLR_FIELDS << {:_available_off => {:as=> :available_off }}
+#  PRODUCT_SOLR_FIELDS << {:store.id => {:as=> :store_id }}
  # PRODUCT_SOLR_FIELDS << :able_off
   #PRODUCT_SOLR_FIELDS << :available_off
   #PRODUCT_SOLR_FIELDS << :deleted_at
   # PRODUCT_SOLR_FIELDS << :store_name
-  #PRODUCT_SOLR_FIELDS << {:store_name => {:type => :integer ,:as =>:supplier_id}}
+  PRODUCT_SOLR_FIELDS << {:store_idds => {:as => :store, :multivalued => true}}
   acts_as_solr  :fields =>  PRODUCT_SOLR_FIELDS  , :facets => PRODUCT_SOLR_FACETS , :include => [
-    {:taxons => {:as => :taxon,  :multivalued => true}} ,
-    {:stores => {:as => :store,  :multivalued => true}} ] 
+    {:taxons => {:as => :taxon,  :multivalued => true}} ] 
   has_and_belongs_to_many :stores, :join_table => 'spree_products_stores'
   scope :by_store, lambda {|store| joins(:stores).where("spree_products_stores.store_id = ?", store)}
   scope :by_available , where( ' ? < available_off' ,Time.now)
@@ -31,6 +31,15 @@ Spree::Product.class_eval do
     self.stores.current.name
     
     
+  end
+  
+  
+  def store_idds
+    
+    @store_ids = []
+    self.stores.each.collect do |store| @store_ids << store.id end
+   
+     @store_ids  
   end
   
 end
