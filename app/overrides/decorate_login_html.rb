5b75b5c85559422a68e301c73272559523d01fcf
TC Juan <tcjuan@localhost.localdomain>
2014-10-17 15:11:21 +0800

    remove filed_tag insertion

--- app/overrides/decorate_login_html.rb
+++ app/overrides/decorate_login_html.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/shared/_login",
                       :name => "insert_store_id_hidden_tag",
                       :insert_top => "#password-credentials",
-                      :text => "<%= f.field_container :store_id do %><%= f.hidden_field :store_id, :value => current_store.id %><% end %>",
+                      :text => "<%= f.hidden_field :store_id, :value => current_store.id %><% end %>",
                       :disabled => false)
                       
 Deface::Override.new(:virtual_path => "spree/shared/_search",
