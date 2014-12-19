b58082d504b8ce15e0c5a0b4b74ecaa1a1a8050d
TC Juan <tcjuan@localhost.localdomain>
2014-12-19 12:11:08 +0800

    modify for default store and default taxonomies

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,3 +1,15 @@
 Spree::Taxonomy.class_eval do
   belongs_to :store
+  
+  
+  scope :default_stores, lambda {|store| joins(:store).where("spree_stores.default = 't' or spree_stores.id = ?" ,store)}
+  
+  
+  def default_taxonomies
+    
+    
+    
+  end
+  
+  
 end
